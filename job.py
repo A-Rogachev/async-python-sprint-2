19a4f7cc4035600044ef4b48b0f562cf295aabd2
Aleksandr Rogachev <alxrogachev@gmail.com>
2023-09-29 22:13:39 +0400

    new_vers

--- job.py
+++ job.py
@@ -1,10 +1,10 @@
 
-import multiprocessing
-from typing import Any, Callable
+# import multiprocessing
+# from typing import Any, Callable
 
-from logging_setup import setup_logger
+# from logging_setup import setup_logger
 
-log = setup_logger(__name__)
+# log = setup_logger(__name__)
 
 
 class Job:
@@ -14,7 +14,7 @@ class Job:
 
     def __init__(
         self,
-        task: Callable[[Any], Any],
+        task,
         args=None,
         kwargs=None,
         start_at='',
@@ -35,38 +35,38 @@ class Job:
         self._dependencies = dependencies or []
         self._status = job_status
 
-    def execute_with_timeout(self, timeout: int):
-        """
-        Выполнение задачи с учетом заданного максимального
-        времени выполнения.
-        """
+#     def execute_with_timeout(self, timeout: int):
+#         """
+#         Выполнение задачи с учетом заданного максимального
+#         времени выполнения.
+#         """
 
-        task_process = multiprocessing.Process(
-            target=self._task,
-            args=self._args,
-            kwargs=self._kwargs,
-        )
-        task_process.start()
-        task_process.join(timeout)
-        if task_process.is_alive():
-            task_process.terminate()
-            task_process.join()
-            raise TimeoutError('Task execution timed out')
+#         task_process = multiprocessing.Process(
+#             target=self._task,
+#             args=self._args,
+#             kwargs=self._kwargs,
+#         )
+#         task_process.start()
+#         task_process.join(timeout)
+#         if task_process.is_alive():
+#             task_process.terminate()
+#             task_process.join()
+#             raise TimeoutError('Task execution timed out')
 
-    def run(self):
-        """
-        Запуск задачи.
-        """
-        try:
-            if self._max_working_time > 0:
-                return self.execute_with_timeout(self._max_working_time)
-            return self._task(*self._args, **self._kwargs)
-        except Exception as job_error:
-            log.error(f'Job error: {job_error}')
-            raise Exception('Job error')
+#     def run(self):
+#         """
+#         Запуск задачи.
+#         """
+#         try:
+#             if self._max_working_time > 0:
+#                 return self.execute_with_timeout(self._max_working_time)
+#             return self._task(*self._args, **self._kwargs)
+#         except Exception as job_error:
+#             log.error(f'Job error: {job_error}')
+#             raise Exception('Job error')
 
-    def __str__(self):
-        """
-        Строковое представление задачи.
-        """
-        return f'<Job {self._task.__name__}, {id(self)}>'
+#     def __str__(self):
+#         """
+#         Строковое представление задачи.
+#         """
+#         return f'<Job {self._task.__name__}, {id(self)}>'
--- main.py
+++ main.py
@@ -14,52 +14,59 @@ def create_tasks_for_scheduler(
     """
     Здесь создаем задачи с рандомным временем.
     """
-    # Обычная функция
-    Job1 = Job(
-        get_world_time,
-        kwargs={'user_timezone': 'europe/samara'},
-    )
-    # Функция с замедлением, в случае увеличения параметра max_working_time
-    # до 3 секунд, не выполняется (TimeoutError)
-    Job2 = Job(
-        get_world_time_slowly,
-        kwargs={'user_timezone': 'europe/moscow'},
-        max_working_time=1,
-    )
-    # Функция с количеством попыток=0, используется чтобы показать,
-    # что функции у которых не осталось попыток на выполнение, завершаются.
-    Job3 = Job(
-        get_world_time,
-        kwargs={'user_timezone': 'europe/saratov'},
-        max_tries=0,
-    )
-    # Стоп-сигнал для планировщика (пример аварийного завершения работы).
-    Job4 = Job(
-        get_world_time_slowly,
-        kwargs={'user_timezone': 'europe/london'},
-        # start_at='29.09.2023 02:28',
-    )
-    Job5 = Job(
-        get_world_time_slowly,
-        kwargs={'user_timezone': 'europe/moscow'},
-        # start_at='29.09.2023 02:28',
-    )
-    Job6 = Job(
-        get_world_time_slowly,
-        kwargs={'user_timezone': 'europe/new_york'},
-        # start_at='29.09.2023 02:28',
-    )
-    Job7 = StopSignal('STOP', scheduler_process)
+    
+    # Job1 = Job(
+    #     get_world_time,
+    #     kwargs={'user_timezone': 'europe/samara'},
+    # )
+    # # Функция с замедлением, в случае увеличения параметра max_working_time
+    # # до 3 секунд, не выполняется (TimeoutError)
+    # Job2 = Job(
+    #     get_world_time_slowly,
+    #     kwargs={'user_timezone': 'europe/moscow'},
+    #     max_working_time=1,
+    # )
+    # # Функция с количеством попыток=0, используется чтобы показать,
+    # # что функции у которых не осталось попыток на выполнение, завершаются.
+    # Job3 = Job(
+    #     get_world_time,
+    #     kwargs={'user_timezone': 'europe/saratov'},
+    #     max_tries=0,
+    # )
+    # # Стоп-сигнал для планировщика (пример аварийного завершения работы).
+    # Job4 = Job(
+    #     get_world_time_slowly,
+    #     kwargs={'user_timezone': 'europe/london'},
+    #     # start_at='29.09.2023 02:28',
+    # )
+    # Job5 = Job(
+    #     get_world_time_slowly,
+    #     kwargs={'user_timezone': 'europe/moscow'},
+    #     # start_at='29.09.2023 02:28',
+    # )
+    # Job6 = Job(
+    #     get_world_time_slowly,
+    #     kwargs={'user_timezone': 'europe/new_york'},
+    #     # start_at='29.09.2023 02:28',
+    # )
+    # Job7 = StopSignal('STOP', scheduler_process)
 
-    for job in (Job1, Job2, Job3, Job4, Job5, Job6, Job7):
-        mng.scheduler.schedule().send(job)
-        if isinstance(job, StopSignal):
-            sleep(6)
-        else:
-            sleep(random.choice([1, 2, 3, 0.5, 2.5, 1.5]))
+    # for job in (Job1, Job2, Job3, Job4, Job5, Job6, Job7):
+    #     mng.scheduler.schedule().send(job)
+    #     if isinstance(job, StopSignal):
+    #         sleep(6)
+    #     else:
+    #         sleep(random.choice([1, 2, 3, 0.5, 2.5, 1.5]))
 
 
 if __name__ == '__main__':
+
+    #####################################################
+    ##### Для запуска планировщика: python3 main.py #####
+    ##### Протестировано на python 3.10.11          #####
+    ##### Использованы только встроенные библиотеки #####
+    #####################################################
+
     task_scheduler = Scheduler(pool_size=3, working_time=20)
     mng = multiprocessing.Manager()
     mng.scheduler: Scheduler = task_scheduler
